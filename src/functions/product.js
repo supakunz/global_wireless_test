77bc1f440382d9f9e39c5a77d65a89d35a3c8e87
Supakun Thata <supakunt.thata@gmail.com>
2024-10-25 17:42:52 +0700

    '#'

--- src/functions/product.js
+++ src/functions/product.js
@@ -2,22 +2,22 @@
 import axios from "axios"
 
 export const create = async (data) => {
-  const response = await axios.post(process.env.NEXT_PUBLIC_API + 'api/product',data)
+  const response = await axios.post('/api/product',data)
   return response;
 }
 export const read = async (id) => {
-  const response = await axios.get(process.env.NEXT_PUBLIC_API + 'api/product/'+ id, id)
+  const response = await axios.get('/api/product/'+ id, id)
   return response;
 }
 export const getdata = async () => {
-  const response = await axios.get(process.env.NEXT_PUBLIC_API + 'api/product')
+  const response = await axios.get('/api/product')
   return response;
 }
 export const update = async (id, data) => {
-  const response = await axios.put(process.env.NEXT_PUBLIC_API + 'api/product/' + id, data)
+  const response = await axios.put('/api/product/' + id, data)
   return response;
 }
 export const remove = async (id) => {
-  const response = await axios.delete(process.env.NEXT_PUBLIC_API + 'api/product/' + id, id)
+  const response = await axios.delete('/api/product/' + id, id)
   return response;
 }
--- src/functions/userdata.js
+++ src/functions/userdata.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const getusers = async () => {
-  const response = await axios.get(process.env.NEXT_PUBLIC_API + 'api/users')
+  const response = await axios.get('/api/users')
   return response;
 }
 
 export const updateusers = async (id, data) => {
-  const response = await axios.put(process.env.NEXT_PUBLIC_API + 'api/users/' + id, data)
+  const response = await axios.put('/api/users/' + id, data)
   return response;
 }
 
 export const removeusers = async (id) => {
-  const response = await axios.delete(process.env.NEXT_PUBLIC_API + 'api/users/' + id, id)
+  const response = await axios.delete('/api/users/' + id, id)
   return response;
 }
