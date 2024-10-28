ae5f0f8ec6cbfd56bec08d7a322f1d151c699ebc
Supakun Thata <supakunt.thata@gmail.com>
2024-10-28 18:14:11 +0700

    test02

--- src/functions/userdata.js
+++ src/functions/userdata.js
@@ -1,16 +1,37 @@
 import axios from "axios";
 
 export const getusers = async () => {
-  const response = await axios.get('/api/users')
+  const response = await axios.get('/api/users',{
+    // query URL without using browser cache
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
+  })
   return response;
 }
 
 export const updateusers = async (id, data) => {
-  const response = await axios.put('/api/users/' + id, data)
+  const response = await axios.put('/api/users/' + id, data,{
+    // query URL without using browser cache
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
+  })
   return response;
 }
 
 export const removeusers = async (id) => {
-  const response = await axios.delete('/api/users/' + id, id)
+  const response = await axios.delete('/api/users/' + id, id,{
+    // query URL without using browser cache
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
+  })
   return response;
 }
